764e3d5db02a99248526022d20d5ddd483f88f3e
diarisdiakite <diarisdiakite@gmail.com>
2023-10-04 01:47:19 +0200

    Add Book class add, list and select methods.

--- classes/book.rb
+++ classes/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :title, :name, :author, :rentals
+  attr_accessor :title, :name, :author, :books, :rentals
   attr_reader :id
+
+  @@books = []
 
   def initialize(title, author)
     @id = generate_id
@@ -11,6 +13,21 @@ class Book
 
   def generate_id
     rand(1_000_000)
+  end
+
+  def self.add_a_book(title, author)
+    book = Book.new(title, author)
+    @@books.push(book)
+  end
+
+  def list_all_books
+    @@books.each { |book| puts "#{book.title} by #{book.author}"}
+  end
+
+  def select_a_book(title_to_select)
+    selected_book = @@books.find { |book| book.title = title_to_select }
+    return selected_book if selected_book
+    puts 'Book not found'
   end
 
   def add_rental(date, person)
