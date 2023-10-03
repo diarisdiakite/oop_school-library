729519ddcd7a2c86de4dc0b84b64a966d31d1b4c
diarisdiakite <diarisdiakite@gmail.com>
2023-10-04 01:48:28 +0200

    Add Rental class add, list and select methods.

--- classes/rental.rb
+++ classes/rental.rb
@@ -2,6 +2,8 @@ class Rental
   attr_accessor :date, :person, :book
   attr_reader :id
 
+  @@rentals = []
+
   def initialize(date, person, book)
     @id = generate_id
     @date = date
@@ -15,5 +17,20 @@ class Rental
 
   def generate_id
     rand(1_000_000)
+  end
+
+  def self.add_a_rental(date, book, person)
+    Rental.new(date, book, person)
+    @@rentals.push(rental)
+  end
+
+  def self.list_all_rentals
+    @@rentals.each { |rental| puts "#{rental.date}: #{rental.book} rented to #{rental.person}" }
+  end
+
+  def self.select_a_rental(rental_to_select)
+    selected_rental = @@rentals.find { |rental| rental.id == rental_to_select }
+    return selected_rental if selected_rental
+    puts "Rental not found"
   end
 end
