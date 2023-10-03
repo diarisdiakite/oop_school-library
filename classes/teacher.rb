ec3951f4705e16c9e2f95c4f90f87eb2af0d46be
diarisdiakite <diarisdiakite@gmail.com>
2023-10-04 01:48:04 +0200

    Add Teacher class add, list and select methods.

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,15 +1,31 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_reader :specialization
+  attr_accessor :specialization, :teachers
 
-  # Constructor extends parent's constructor by adding @specialization and a parameter for it.
+  # My class variable teachers to store all the teachers
+  @@teachers = []
+
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
 
-  # Override can_use_services? so it always returns true
+  def self.add_a_teacher(age, specialization, name = 'Unknown')
+    teacher = Teacher.new(age, specialization, name)
+    @@teachers.push(teacher)
+  end
+
+  def self.list_all_teachers
+    @@teachers.each { |teacher| puts "#{teacher.name}, specialized in #{teacher.specialization}" }
+  end
+
+  def self.select_a_teacher(teacher_to_select)
+    selected_teacher = @@teachers.find { |teacher| teacher.name === teacher_to_select }
+    return selected_teacher if selected_teacher
+    puts 'Teacher not found'
+  end
+
   def can_use_services?
     true
   end
