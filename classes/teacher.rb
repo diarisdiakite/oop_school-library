c37882135376a4f9ab3cae8f0ca3019c109f69b0
diarisdiakite <diarisdiakite@gmail.com>
2023-10-04 17:52:14 +0200

    Change the class variable teachers to class intance variable. Added a self.class in the initialize method. Added the self.teachers method and a teachers count method.

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -4,25 +4,34 @@ class Teacher < Person
   attr_accessor :specialization, :teachers
 
   # My class variable teachers to store all the teachers
-  @@teachers = []
+  @teachers = []
 
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', specialization, parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
+    self.class.teachers << self
   end
 
-  def self.add_a_teacher(age, specialization, name = 'Unknown')
-    teacher = Teacher.new(age, specialization, name)
-    @@teachers.push(teacher)
+  def self.add_a_teacher(age, name = 'Unknown', specialization)
+    teacher = Teacher.new(age, name, specialization)
+  end
+
+  def self.teachers
+    @teachers
+  end
+
+  def self.teachers_count
+    @teachers.length
   end
 
   def self.list_all_teachers
-    @@teachers.each { |teacher| puts "#{teacher.name}, specialized in #{teacher.specialization}" }
+    @teachers.each { |teacher| puts "[#{teacher.id}] #{teacher.name}, specialized in #{teacher.specialization}" }
   end
 
   def self.select_a_teacher(teacher_to_select)
-    selected_teacher = @@teachers.find { |teacher| teacher.name === teacher_to_select }
+    selected_teacher = @teachers.find { |teacher| teacher.name == teacher_to_select }
     return selected_teacher if selected_teacher
+
     puts 'Teacher not found'
   end
 
