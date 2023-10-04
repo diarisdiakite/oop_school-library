3e1c030c37729becab6480d1022cb9eb88c752cd
diarisdiakite <diarisdiakite@gmail.com>
2023-10-04 17:49:54 +0200

    Change the class variable books to class intance variable. Added a self.class in the initialize method. Added the self.books method and a books count method.

--- classes/book.rb
+++ classes/book.rb
@@ -1,14 +1,15 @@
 class Book
-  attr_accessor :title, :name, :author, :books, :rentals
+  attr_accessor :title, :name, :author, :rentals
   attr_reader :id
 
-  @@books = []
+  @books = []
 
   def initialize(title, author)
     @id = generate_id
     @title = title
     @author = author
     @rentals = []
+    self.class.books << self
   end
 
   def generate_id
@@ -17,20 +18,28 @@ class Book
 
   def self.add_a_book(title, author)
     book = Book.new(title, author)
-    @@books.push(book)
   end
 
-  def list_all_books
-    @@books.each { |book| puts "#{book.title} by #{book.author}"}
+  def self.list_all_books
+    @books.each { |book| puts "[#{book.id}] #{book.title} by #{book.author}" }
   end
 
   def select_a_book(title_to_select)
-    selected_book = @@books.find { |book| book.title = title_to_select }
+    selected_book = @books.find { |book| book.title == title_to_select }
     return selected_book if selected_book
+
     puts 'Book not found'
   end
 
   def add_rental(date, person)
     Rental.new(date, self, person)
+  end
+
+  def self.books
+    @books
+  end
+
+  def self.books_count
+    @books.length
   end
 end
